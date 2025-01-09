dff7811267042e197102070a79b2573064a09988
Rebi Edward Akpa <rebifaizahedward_akpa@outlook.com>
2025-01-09 18:49:42 +0100

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log('config.js');
-console.log('version1');
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+ res.statusCode = 200;
+ res.setHeader("Content-Type", "test/plain");
+ res.end("Hello World xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://$(hostname):$(port)/`);
+})
